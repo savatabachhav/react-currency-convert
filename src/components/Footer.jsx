bf5679f1bffdb30168c12ecc289e45fb82efc222
Savata Bachhav <savatabachhav112@gmail.com>
2023-12-27 13:07:31 +0530

    title update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   return (
     <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-300">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span class="text-sm text-gray-200 sm:text-center dark:text-gray-950">© 2023 
-                <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+            <span class="text-sm text-gray-200 sm:text-center dark:text-gray-950">© 2024 
+              <a href="https://www.linkedin.com/in/savatabachhav" className="hover:underline">Savata Bachhav</a>
             </span>
         </div>
     </footer>
