93ac914c8cac53ddbcef3b0ef2c50acc9ae46c81
savatabachhav <34607163+savatabachhav@users.noreply.github.com>
2023-12-28 20:19:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 import {InputBox} from './components/index.js'
