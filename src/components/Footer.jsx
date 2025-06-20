a04d067aadf21cba9491606a84c4273503dfe869
Savata Bachhav <34607163+savatabachhav@users.noreply.github.com>
2025-06-20 11:19:51 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-300">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
             <span class="text-sm text-gray-200 sm:text-center dark:text-gray-950">© 2024 
-              <a href="https://www.linkedin.com/in/savatabachhav" className="hover:underline">Savata Bachhav</a>
+              <a href="https://www.linkedin.com/in/savatabachhav" className="hover:underline">Savata B</a>
             </span>
         </div>
     </footer>
